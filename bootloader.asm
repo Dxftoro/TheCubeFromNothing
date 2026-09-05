0713f86db861b835ee82ff4ec8b9603a1e7c53ba
Ftoro <lesnik.antonio@gmail.com>
2026-09-05 10:40:48 +0300

    feat: draw function

--- bootloader.asm
+++ bootloader.asm
@@ -4,6 +4,9 @@ org 0x7C00
 S_WIDTH 		= 80
 S_HEIGHT 		= 25
 VIDEO_MEM		= 0xB800
+DIST			= 64
+CENTER_X		= 12
+CENTER_Y		= 12
 
 ; =======================================================
 ; Colors
@@ -28,7 +31,7 @@ COLOR_WHITE			= 0x0F
 ; =======================================================
 ; Macros
 ; =======================================================
-macro putchar x, y, char, attr {
+macro cputchar x, y, char, attr {
 	mov di, (S_WIDTH * y + x) * 2
 	mov byte [es:di], char
 	mov byte [es:di + 1], attr
@@ -55,18 +58,44 @@ start:
 ; Functions
 ; =======================================================
 
+; Perspective projection:
+; sx = x * DIST / (z + DIST) + CENTER_X
+; sy = y * DIST / (z + DIST) + CENTER_Y
 draw_vertices:
 	mov cx, 0
+	mov si, cube_vertices
 	
-	.vert_pass:	cmp cx, 7
-		mov ax, [cube_x + cx] 
-		cwd
-		imul dist
+	.vert_pass:	cmp cx, 8
+		; Zd = z + DIST, Zd -> bx
+		mov al, [si + 2]
+		cbw
+		add ax, DIST
+		mov bx, ax
 		
+		; x * DIST / Zd + CENTER_X
+		mov al, [si]
+		cbw
+		imul ax, ax, DIST
+		cwd
+		idiv bx
+		add ax, CENTER_X
+		mov [screen_coords], al
+		
+		; y * DIST / Zd + CENTER_Y
+		mov al, [si + 1]
+		cbw
+		imul ax, ax, DIST
+		cwd
+		idiv bx
+		add ax, CENTER_Y
+		mov [screen_coords], al
+		
+		putchar [screen_coords], [screen_coords + 1], '#', COLOR_LIGHT_BLUE
+		
+		add si, 3
 		inc cx
 		jne .vert_pass
-	
-	putchar 10, 10, '#', COLOR_LIGHT_BLUE
+	ret
 
 ; args: byte angle_index (al)
 tsin:
@@ -113,13 +142,12 @@ sin_table:
 	db 0x00,0xEC,0xDA,0xC8,0xB9,0xAD,0xA4,0x9E
 	db 0x9C,0x9E,0xA4,0xAD,0xB9,0xC8,0xDA,0xEC
 
-cube_x	db 10, 10, 10, 10, -10, -10, -10, -10
-cube_y	db 10, 10, -10, -10, 10, 10, -10, -10
-cube_z	db 10, -10, 10, -10, 10, -10, 10, -10
+cube_vertices	db 10, 10, 10,	10, 10, -10,
+				db 10, -10, 10,	10, -10, -10,
+				db -10, 10, 10,	-10, 10, -10,
+				db -10, -10, 10, -10, -10, -10
 
-screen_x	db 0
-screen_y	db 0
-dist 		db 10
+screen_coords	db 0, 0
 
 times 510-($-$$) db 0
 dw 0xAA55
