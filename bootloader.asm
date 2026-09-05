688de023c9e8e44bda08af0a6cd67c368ad26ca2
Ftoro <lesnik.antonio@gmail.com>
2026-09-05 12:50:58 +0300

    feat: vertex rotation

--- bootloader.asm
+++ bootloader.asm
@@ -5,8 +5,9 @@ S_WIDTH 		= 80
 S_HEIGHT 		= 25
 VIDEO_MEM		= 0xB800
 DIST			= 64
-CENTER_X		= 12
+CENTER_X		= 40
 CENTER_Y		= 12
+SINE_TALBE_AMP	= 100
 
 ; =======================================================
 ; Colors
@@ -31,10 +32,23 @@ COLOR_WHITE			= 0x0F
 ; =======================================================
 ; Macros
 ; =======================================================
+
 macro putchar x, y, char, attr {
 	mov di, (S_WIDTH * y + x) * 2
 	mov byte [es:di], char
 	mov byte [es:di + 1], attr
+}
+
+macro delay dhi, dlo {
+	mov ah, 0x86
+	mov cx, dhi
+	mov dx, dlo
+	
+	int 15h
+
+	xor ah, ah
+	xor cx, cx
+	xor dx, dx
 }
 
 ; =======================================================
@@ -45,27 +59,93 @@ start:
 	mov ds, ax
 	mov es, ax
 	
-	call clear_screen
+	.main_loop:
+		call clear_screen
 	
-	mov ax, VIDEO_MEM
-	mov es, ax
+		mov ax, VIDEO_MEM
+		mov es, ax
+			
+		call rotate_cube
+		call draw_vertices
 	
-	call draw_vertices
+		; 1ms delay
+		delay 0x0000, 0x03E8
 	
-	jmp $
+		jmp .main_loop
 
 ; =======================================================
 ; Functions
 ; =======================================================
 
+rotate_cube:
+	mov cx, 8
+	mov si, cube_vertices
+	mov di, rotated_verticies
+	
+	.rotation_pass:
+		call rotate_vertex
+		
+		; x
+		mov [di], al
+		; y
+		mov [di + 1], bh
+		; z
+		mov [di + 2], bl
+		
+		add si, 3
+		add di, 3
+		loop .rotation_pass
+	
+	ret
+
+; ret: x (al), y (bh), z (bl)
+; rot_x = (x * cos(angle) - z * sin(angle)) / SINE_TALBE_AMP
+; rot_z = (x * sin(angle) + z * cos(angle)) / SINE_TALBE_AMP
+rotate_vertex:
+	mov al, [current_angle]
+	call tsin
+	mov [sinv], al
+	
+	mov al, [current_angle]
+	call tcos
+	mov [cosv], al
+	
+	; rotating x
+	mov al, [si]
+	imul byte [cosv]
+	mov bx, ax
+	mov al, [si + 2]
+	imul byte [sinv]
+	sub bx, ax
+	mov ax, bx
+	cwd
+	idiv word [SINE_TALBE_AMP]
+	
+	; rotating z
+	mov al, [si]
+	imul byte [sinv]
+	mov bx, ax
+	mov al, [si + 2]
+	imul byte [cosv]
+	add bx, ax
+	mov ax, bx
+	cwd
+	idiv word [SINE_TALBE_AMP]
+	mov bl, al
+	
+	; rotating y (do not)
+	mov bh, [si + 1]
+	
+	ret
+
 ; Perspective projection:
 ; sx = x * DIST / (z + DIST) + CENTER_X
 ; sy = y * DIST / (z + DIST) + CENTER_Y
 draw_vertices:
-	mov cx, 0
-	mov si, cube_vertices
+	mov cx, 8
+	mov si, rotated_verticies
 	
-	.vert_pass:	cmp cx, 8
+	.vert_pass:
 		; Zd = z + DIST, Zd -> bx
 		mov al, [si + 2]
 		cbw
@@ -88,16 +168,14 @@ draw_vertices:
 		cwd
 		idiv bx
 		add ax, CENTER_Y
-		mov [screen_coords], al
+		mov [screen_coords + 1], al
 		
-		; putchar [screen_coords], [screen_coords + 1], '#', COLOR_LIGHT_BLUE
-		mov bl, '#',
+		mov bl, '#'
 		mov bh, COLOR_LIGHT_BLUE
 		call draw_pixel
 		
 		add si, 3
-		inc cx
-		jne .vert_pass
+		loop .vert_pass
 	ret
 
 ; Draws a pixel with coordinates stored in screen_coords
@@ -119,12 +197,14 @@ draw_pixel:
 	ret
 	
 ; args: byte angle_index (al)
+; ret: sin value (al)
 tsin:
 	mov bx, ax
 	mov al, [sin_table + bx]
 	ret
 
 ; args: byte angle_index (al)
+; ret: cos value (al)
 tcos:
 	add al, 8
 	and al, 31
@@ -163,12 +243,16 @@ sin_table:
 	db 0x00,0xEC,0xDA,0xC8,0xB9,0xAD,0xA4,0x9E
 	db 0x9C,0x9E,0xA4,0xAD,0xB9,0xC8,0xDA,0xEC
 
-cube_vertices	db 10, 10, 10,	10, 10, -10,
-				db 10, -10, 10,	10, -10, -10,
-				db -10, 10, 10,	-10, 10, -10,
-				db -10, -10, 10, -10, -10, -10
+cube_vertices	db 10, 10, 10,		10, 10, -10
+				db 10, -10, 10,		10, -10, -10
+				db -10, 10, 10,		-10, 10, -10
+				db -10, -10, 10, 	-10, -10, -10
 
-screen_coords	db 0, 0
+rotated_verticies rb 8 * 3
+screen_coords		db 0, 0
+current_angle		db 0
+sinv				db 0
+cosv				db 0
 
 times 510-($-$$) db 0
 dw 0xAA55
