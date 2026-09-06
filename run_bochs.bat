1094affd8c07d235ebaca47f09fe84b13a52e630
Ftoro <lesnik.antonio@gmail.com>
2026-09-06 15:14:03 +0300

    build: moved all source code to src dir

--- run_bochs.bat
+++ run_bochs.bat
@@ -2,8 +2,8 @@
 
 mkdir bin 2>nul
 
-fasm bootloader.asm bin/bootloader.bin
-fasm kernel.asm bin/kernel.bin
+fasm src/bootloader.asm bin/bootloader.bin
+fasm src/kernel.asm bin/kernel.bin
 copy /b bin\bootloader.bin + bin\kernel.bin bin\cubefn.bin
 
 bochs -f cubefn.txt
--- run_qemu.bat
+++ run_qemu.bat
@@ -2,8 +2,8 @@
 
 mkdir bin 2>nul
 
-fasm bootloader.asm bin/bootloader.bin
-fasm kernel.asm bin/kernel.bin
+fasm src/bootloader.asm bin/bootloader.bin
+fasm src/kernel.asm bin/kernel.bin
 copy /b bin\bootloader.bin + bin\kernel.bin bin\cubefn.bin
 
 qemu-system-x86_64 -drive file=bin/cubefn.bin,format=raw,if=ide
