77de80f5adc0d7d44d119c5973af0350ac5c7693
Ftoro <lesnik.antonio@gmail.com>
2026-09-06 12:12:40 +0300

    fix: 'copy' command syntax

--- run_qemu.bat
+++ run_qemu.bat
@@ -4,6 +4,6 @@ mkdir bin 2>nul
 
 fasm bootloader.asm bin/bootloader.bin
 fasm kernel.asm bin/kernel.bin
-copy /b bin\bootloader.bin + bin\kernel.bin bin/cubefn.bin
+copy /b bin\bootloader.bin + bin\kernel.bin bin\cubefn.bin
 
 qemu-system-x86_64 -drive file=bin/cubefn.bin,format=raw,if=ide
